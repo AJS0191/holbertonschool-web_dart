9afe4fba4555d13ffc800b61659f7c1909b30358
AJS0191 <ashtonjamess@live.com>
2023-02-21 09:21:16 -0600

    remove extra newline

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,7 +1,7 @@
 void outer(String name, String id){
   String agent = name.split(' ')[1][0] + '.' + name.split(' ')[0];
   String inner() {
-    return('Hello Agent $agent your id is $id\n');
+    return('Hello Agent $agent your id is $id');
   }
   print(inner());
 }
