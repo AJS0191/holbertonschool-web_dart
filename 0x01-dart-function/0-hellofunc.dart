2e7c1baed800e00191dfa48d4528554a6f380f6e
AJS0191 <ashtonjamess@live.com>
2023-02-21 08:49:28 -0600

    outside of a main

--- 0x01-dart-function/0-hellofunc.dart
+++ 0x01-dart-function/0-hellofunc.dart
@@ -1,6 +1,3 @@
-void main() {
-  void helloFunction(String str){
-    print('Hello $str from dart');
-  }
-  helloFunction('Holberton');
+helloFunction(String str){
+  print('Hello $str from dart');
 }
