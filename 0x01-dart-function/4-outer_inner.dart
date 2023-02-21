04d5499c6f9ae58b248affe9b4c06eefd0f13010
AJS0191 <ashtonjamess@live.com>
2023-02-21 09:20:30 -0600

    correct letter

--- 0x01-dart-function/4-outer_inner.dart
+++ 0x01-dart-function/4-outer_inner.dart
@@ -1,5 +1,5 @@
 void outer(String name, String id){
-  String agent = name.split(' ')[1][1] + '.' + name.split(' ')[0];
+  String agent = name.split(' ')[1][0] + '.' + name.split(' ')[0];
   String inner() {
     return('Hello Agent $agent your id is $id\n');
   }
