ea2bfa50e987d9056eb3df1d4490bf16a66b0b6f
AJS0191 <ashtonjamess@live.com>
2023-02-22 10:15:38 -0600

    basketball score keeper

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,9 @@
+int whoWins(Map<String, int> team1, Map<String, int> team2) {
+  var score1 = (team1['2 pointers']! * 2) + team1['Free throws']! + (team1['3 pointers']! * 3);
+  var score2 = (team2['2 pointers']! * 2) + team2['Free throws']! + (team2['3 pointers']! * 3);
+  if (score1 > score2) {
+    return 1;
+  } else {
+    return 2;
+  }
+}
