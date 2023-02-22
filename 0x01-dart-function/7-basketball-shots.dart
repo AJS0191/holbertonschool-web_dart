46a9822088721c4cf203927c8026d20a2ad0de0b
AJS0191 <ashtonjamess@live.com>
2023-02-22 10:18:00 -0600

    account for ties

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -3,6 +3,8 @@ int whoWins(Map<String, int> team1, Map<String, int> team2) {
   var score2 = (team2['2 pointers']! * 2) + team2['Free throws']! + (team2['3 pointers']! * 3);
   if (score1 > score2) {
     return 1;
+  } else if (score1 == score2) {
+    return 0;
   } else {
     return 2;
   }
