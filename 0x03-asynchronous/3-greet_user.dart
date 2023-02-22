6285dac419689984805220dd34eb898b859c8e1c
AJS0191 <ashtonjamess@live.com>
2023-02-22 15:12:21 -0600

    a little of this and that

---
+++ 0x03-asynchronous/3-greet_user.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+
+
+Future<bool> checkCredentials() =>
+    Future.delayed(const Duration(seconds: 2), () => true);
+main() async {
+  print(await loginUser());
+}
+
+Future<String> fetchUserData() => Future.delayed(
+      const Duration(seconds: 2),
+      () =>
+          '{"id" : "7ee9a243-01ca-47c9-aa14-0149789764c3", "username" : "admin"}',
+    );
+
+Future<String> greetUser() async {
+  final String userData = await fetchUserData();
+  final Map<String, dynamic> user = jsonDecode(userData);
+  return 'hello ${user['name']}';
+}
+
+Future<String> loginUser() async {
+  try {
+    final bool credentials = await checkCredentials();
+    if (credentials) {
+      return greetUser();
+    } else {
+      return 'Wrong credentials';
+    }
+  } catch (e) {
+    return 'error caught: $e';
+  }
+}
