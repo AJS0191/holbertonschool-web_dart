e868981869bedcfea71e3ad45f592f1be841f511
AJS0191 <ashtonjamess@live.com>
2023-02-22 13:31:23 -0600

    twofor

---
+++ 0x02-dart-classes/6-inheritance.dart
@@ -0,0 +1,34 @@
+import '6-password.dart';
+
+class User extends Password{
+  String name = '';
+  int age = 0;
+  double height = 0.0;
+  var id;
+  Password user_password;
+
+  User({this.id, required this.name, required this.age, required this.height, required this.user_password}) : super(password: user_password.toString());
+
+  //bool passValid = user_password.isValid();
+  Map toJson() {
+    return {
+      'id': id, 'name': name, 'age': age, 'height': height
+    };
+  }
+
+  static User fromJson(Map json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      age: json['age'],
+      height: json['height'],
+      user_password: json['user_password']
+    );
+
+  }
+
+
+  String toString() {
+    return 'User(id : $id ,name: $name,  age: $age, height: $height, Password: ${user_password.isValid()})';
+  }
+}
